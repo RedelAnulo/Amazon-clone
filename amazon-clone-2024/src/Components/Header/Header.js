493b80c808c6a0664c5e0a5b292209cd91bc3e1f
Redela Anulo <84re65de01la60@gmail.com>
2024-05-18 15:11:16 +0300

    First commit for Header

--- amazon-clone-2024/src/Components/Header/Header.js
+++ amazon-clone-2024/src/Components/Header/Header.js
@@ -1,74 +1,73 @@
-import React from 'react'
+import React from "react";
 import { BsSearch } from "react-icons/bs";
 import { BiCart } from "react-icons/bi";
 import { SlLocationPin } from "react-icons/sl";
+import classes from "./Header.module.css";
 
 function Header() {
-  return (
-    <div>
-      <section>
-      <section>
-        <div>
-            {/* Logo */}
-            <a href=''>
-                <img src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'alt='Amazon Logo'/>
-            </a>
-            {/* Delivery */}
-            <span>
-                {/* Icon */}
-                <SlLocationPin/>
-            </span>
-            <div>
-                <p>Delivered to</p>
-                <span>Ethiopia</span>
-            </div>
-       </div>
-        <div>
-            {/* Search */}
-            <select name='' id=''>
-              <option value=''>All</option>
-            </select>
-            <input type='text' name='' id='' placeholder='Search Product'/>
-            {/* Icon */}
-            <BsSearch/>
-        </div>
-        {/* right side link */}
-        <div>
+	return (
+		<>
+			<section>
+				<div className={classes.header__container}>
+					{/* Logo Section*/}
+					<div className={classes.logo__container}>
+						<a href="#">
+							<img
+								src="https://pngimg.com/uploads/amazon/small/amazon_PNG11.png"
+								alt="Amazon Logo"
+							/>
+						</a>
+						<div className={classes.delivery}>
+							{/* Delivery */}
+							<span>
+								{/* Icon */}
+								<SlLocationPin />
+							</span>
+							<div>
+								<p>Delivered to</p>
+								<span>Ethiopia</span>
+							</div>
+						</div>
+					</div>
 
-
-          <div>
-            <img src='https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg' alt='Flag'/>
-              <select>
-                <option value=''>EN</option>
-              </select>
-          </div>
-          {/* Three components */}
-          <a href=''>
-            <div>
-              <p>Sign In</p>
-              <span>Accounts & Lists</span>
-            </div>
-          </a>
-          {/* Orders */}
-          <a>
-            <p>returns</p>
-            <span> & Orders</span>
-          </a>
-          {/* Cart */}
-          <a to='/cart'>
-            {/* icon */}
-<BiCart/>
-            <span>0</span>
-          </a>
-
-
-        </div>
-
-      </section>
-
-      </section>
-    </div>
-  )
+					{/* Search Section*/}
+					<div className={classes.search}>
+						<select name="" id="">
+							<option value="">All</option>
+						</select>
+						<input type="text" name="" id="" placeholder="Search Product" />
+						{/* Icon */}
+						<BsSearch size={25} />
+					</div>
+					{/* other section */}
+					<div className={classes.order__container}>
+						<a href="#" className={classes.language}>
+							<img 
+								src="https://canadianaflag.ca/cdn/shop/files/USA.jpg?crop=center&height=1080&v=1713209885&width=1080"
+							 	alt="Flag"
+							/>
+							<select>
+								<option value="">EN</option>
+							</select>
+						</a>
+						<a href="#">
+							<p>Sign In</p>
+							<span>Accounts & Lists</span>
+						</a>
+						<a>
+							<p>returns</p>
+							<span> & Orders</span>
+						</a>
+						<a href="" className={classes.cart}>
+							{/* icon */}
+							<BiCart size={35} />
+							<span>0</span>
+						</a>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
 
-export default Header
+export default Header;
