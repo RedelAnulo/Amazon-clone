e7633ea3706ecc029026a33bcff725f1b4607e90
Redela Anulo <84re65de01la60@gmail.com>
2024-05-19 15:01:34 +0300

    Carousel and Category

--- amazon-clone-2024/src/App.js
+++ amazon-clone-2024/src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import Header from './Components/Header/Header';
+import "./App.css";
+import CarouselEffect from "./Components/Carousel/Carousel";
+import Category from "./Components/Category/Category";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<CarouselEffect />
+			<Category />
+		</div>
+	);
 }
 
 export default App;
